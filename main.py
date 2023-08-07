841f4f9744b528ad075a19081a41d421de2bf71f
pschlo <>
2023-08-07 13:32:11 +0200

    trying to completely rework and clean up the code

--- main.py
+++ main.py
@@ -1,159 +1,284 @@
 import pygame as pg
 import time
 import sys
+import math
+from abc import ABC, abstractmethod
 
 pg.init()
-WIN = pg.display.set_mode((1200, 640))
-FONT_LARGE = pg.font.SysFont('monospace', 32, bold=False)
-FONT_SMALL = pg.font.SysFont('monospace', 18)
 
-exercise_text = r'Returns the dimensions needed to render the text. This can be used to help determine the positioning needed for text before it is rendered. It can also be used for wordwrapping and other layout effects.'
+class Score:
+    errors: int
+    chars_per_minute: int
 
 
-class TextBox:
-    def __init__(self, exercise_text):
-        self.COLOR_TEXT_NORMAL = pg.Color('black')
-        self.COLOR_TEXT_ACTIVE = pg.Color('green4')
-        self.COLOR_TEXT_MISTYPED = pg.Color('red')
-        self.COLOR_RECT = pg.Color('black')
-        self.PADDING_X = 40
-        self.PADDING_Y = 5
-        self.CPM_COOLDOWN = 1
-        
+
+
+class SceneManager:
+    pass
+
+
+class Scene(ABC):
+    manager: SceneManager
+
+    def __init__(self, manager:SceneManager) -> None:
+        self.manager = manager
+
+    @abstractmethod
+    def render(self, screen:pg.Surface) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def update(self) -> None:
+        pass
+
+    @abstractmethod
+    def handle_events(self, events:list[pg.event.Event]) -> None:
+        raise NotImplementedError
+
+
+class IngameScene(Scene):
+    def __init__(self, manager: SceneManager) -> None:
+        super().__init__(manager)
+
+
+
+
+class A(pg.surface.Surface):
+    def __init__(self, size: tuple[int,int]):
+        super().__init__(size)
+
+
+
+
+class GameOptions:
+    def __init__(
+        self,
+        COLOR_TEXT_NORMAL: pg.Color = pg.Color('black'),
+        COLOR_TEXT_ACTIVE: pg.Color = pg.Color('green4'),
+        COLOR_TEXT_MISTYPED: pg.Color = pg.Color('red'),
+        COLOR_RECT: pg.Color = pg.Color('black'),
+
+        PADDING_X: int = 40,
+        PADDING_Y: int = 5,
+        CPM_COOLDOWN: int = 1,
+
+        LEN_TEXT_DISPLAYED: int = 41,
+        FONT_LARGE: pg.font.Font = pg.font.SysFont('monospace', 32, bold=False),
+        FONT_SMALL: pg.font.Font = pg.font.SysFont('monospace', 18),
+    ) -> None:
+        self.COLOR_TEXT_NORMAL = COLOR_TEXT_NORMAL
+        self.COLOR_TEXT_ACTIVE = COLOR_TEXT_ACTIVE
+        self.COLOR_TEXT_MISTYPED = COLOR_TEXT_MISTYPED
+        self.COLOR_RECT = COLOR_RECT
+
+        self.PADDING_X = PADDING_X
+        self.PADDING_Y = PADDING_Y
+        self.CPM_COOLDOWN = CPM_COOLDOWN
+
         # LEN_TEXT_DISPLAYED must be odd so that the cursor letter is always in the middle
-        self.LEN_TEXT_DISPLAYED = 41
+        self.LEN_TEXT_DISPLAYED = LEN_TEXT_DISPLAYED
         assert self.LEN_TEXT_DISPLAYED % 2 == 1
+
+        self.FONT_LARGE = FONT_LARGE
+        self.FONT_SMALL = FONT_SMALL
+
+
+
+class TextSurface:
+    parts: tuple[pg.Surface]
+
+    def __init__(self, *parts: pg.Surface) -> None:
+        self.parts = parts
+
+    def draw(self, surface: pg.Surface, dest: tuple[int,int]):
+        x = dest[0]
+        y = dest[1]
+        for part in self.parts:
+            surface.blit(part, (x,y))
+            x += part.get_width()
+
+
+# represents a rectangle that contains text
+class TextBox:
+    text: str
+    rect: pg.Rect
+    options: GameOptions
+
+    def __init__(self, text:str, rect:pg.Rect, options:GameOptions) -> None:
+        self.text = ' ' * options.LEN_TEXT_DISPLAYED + text
+        self.cursor = options.LEN_TEXT_DISPLAYED + 1
+        self.rect = rect
+        self.options = options
+
+    def draw(self, on_surface:pg.Surface, from_surface:TextSurface) -> None:
+        # draw rectange
+        pg.draw.rect(on_surface, self.options.COLOR_RECT, self.rect, 2)
+        # draw text
+        dest = (self.rect.x + self.options.PADDING_X, self.rect.y + self.options.PADDING_Y)
+        from_surface.draw(on_surface, dest)
+
+    def render_normal(self, text:str) -> TextSurface:
+        surface = self.options.FONT_LARGE.render(text, True, self.options.COLOR_TEXT_NORMAL)
+        return TextSurface(surface)
+
+    def render_exercise(self, cursor:int) -> TextSurface:
+        surfaces: list[pg.Surface] = []
+
+        # parse left part
+        length = math.floor((self.options.LEN_TEXT_DISPLAYED + 1) / 2) - 1
+        index_left = cursor - length
+        index_right = cursor - 1
+
+        # 4: length = floor((4+1)/2) - 1 = floor(2.5) - 1 = 2-1 = 1
+        # 5: length = floor((5+1)/2) - 1 = floor(3) - 1 = 3-1 = 2
+        # 6: length = floor((6+1)/2) - 1 = floor(3.5) - 1 = 3-1 = 2
+        surface = self.options.FONT_LARGE.render(self.text[index_left : index_right+1], True, self.options.COLOR_TEXT_NORMAL)
+        surfaces.append(surface)
+
+        # parse center part
+
+        surface = self.options.FONT_LARGE.render(self.text[cursor], True, self.options.COLOR_TEXT_NORMAL)
+        surfaces.append(surface)
+
+
+        # parse right part
+        length = math.floor((self.options.LEN_TEXT_DISPLAYED + 1) / 2)
+        index_left = cursor + 1
+        index_right = cursor + length
+
+        surface = self.options.FONT_LARGE.render(self.text[index_left : index_right+1], True, self.options.COLOR_TEXT_NORMAL)
+        surfaces.append(surface)
+
+        # 4: length = floor((4+1)/2) = floor(2.5) = 2
+        # 5: length = floor((5+1)/2) = floor(3) = 3
+        # 6: length = floor((6+1)/2) = floor(3.5) = 3
+
+        return TextSurface(*surfaces)
+
+
+
+
+
+class Game:
+
+    options: GameOptions
+    text_exercise: str
+    clock: pg.time.Clock
+
+    TEXT_START = r'Press <Space> to start'
+    TEXT_FINISH = 'Exercise finished'
+
+    time_start: float
+    cursor = 0
+    errors = 0
+    chars_per_minute: int = 0
+    is_mistyped: bool = False
+    has_started: bool = False
+    last_cpm_refresh = 0
+
+
+    def __init__(self, text:str, screen:pg.Surface, options:GameOptions):
+        self.options = options
+        self.text_exercise = text
+        self.screen = screen
+
+        self.clock = pg.time.Clock()
         
-        SIZE_TEXT = FONT_LARGE.size('a'*self.LEN_TEXT_DISPLAYED)
-        RECT_WIDTH = SIZE_TEXT[0] + 2*self.PADDING_X
-        RECT_HEIGHT = SIZE_TEXT[1] + 2*self.PADDING_Y
-        WIN_WIDTH, WIN_HEIGHT = WIN.get_size()
-        
-        self.RECT = pg.Rect(
-            int((WIN_WIDTH-RECT_WIDTH)/2),
-            int((WIN_HEIGHT-RECT_HEIGHT)/2)-50,
+        # the rectange where the text should appear
+
+        SIZE_TEXT = self.options.FONT_LARGE.size('a'*self.options.LEN_TEXT_DISPLAYED)
+        RECT_WIDTH = SIZE_TEXT[0] + 2*self.options.PADDING_X
+        RECT_HEIGHT = SIZE_TEXT[1] + 2*self.options.PADDING_Y
+
+        WIN_WIDTH, WIN_HEIGHT = screen.get_size()
+
+        rect = pg.Rect(
+            int((WIN_WIDTH-RECT_WIDTH)/2),          # horizonal axis: centered
+            int((WIN_HEIGHT-RECT_HEIGHT)/2)-50,     # vertical axis: little bit above center
             RECT_WIDTH,
             RECT_HEIGHT
         )
-        
-        self.EXERCISE_TEXT = exercise_text
-        
-        self.text = r'Press <Space> to start'
-        self.list_surf_text = []
-        self.cursor = 0
-        self.errors = 0
-        self.cpm = None
-        self.is_mistyped = False
-        self.has_started = False
-        self.time_start = None
-        self.last_cpm_refresh = 0
-        
-        
-    def update(self, events):
 
-        if self.has_started and time.time() - self.last_cpm_refresh > self.CPM_COOLDOWN:
-            self.last_cpm_refresh = time.time()
-            # calculate clicks per minute
-            delta_t = time.time() - self.time_start
-            self.cpm = int( self.cursor / (delta_t/60) )
+        self.textbox = TextBox(self.text_exercise, rect, options)
+    
 
-        print("get_mods =", pg.key.get_mods())
-        print("self.is_mistyped", self.is_mistyped)
+    def start(self):
+        ## event loop
+        while True:
+            self.screen.fill(pg.Color('white'))
+            self.update()
             
-        for event in events:
-            if event.type == pg.KEYDOWN:
-                if not self.has_started:
-                    if event.key == pg.K_SPACE:
-                        # start exercise
-                        self.has_started = True
-                        self.text = self.EXERCISE_TEXT
-                        self.time_start = time.time()
-                else:   
-                    if event.unicode == self.text[self.cursor]:
-                        # correct key
-                        if self.cursor == len(self.EXERCISE_TEXT) - 1:
-                            self.text = 'Exercise finished'
-                            self.has_started = False
-                        self.cursor += 1
-                        self.is_mistyped = False
-                    elif not (pg.key.get_mods() or event.unicode == '' or self.is_mistyped):
-                        # mistyped
-                        self.is_mistyped = True
-                        self.errors += 1
+            #self.screen.blit(self.options.FONT_SMALL.render('Errors: %d' % self.errors, True, self.options.COLOR_TEXT_NORMAL), (self.rect.x, game.RECT.bottom+50))
+            #self.screen.blit(self.options.FONT_SMALL.render('Characters per minute: %s' % self.chars_per_minute, True, game.options.COLOR_TEXT_NORMAL), (game.RECT.x + 160, game.RECT.bottom+50))
+            
+            pg.display.flip()
+            self.clock.tick(60)
         
+
+    def update(self):
+        def is_quit(event: pg.event.Event):
+            return event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE)
+        
+        def is_start(event: pg.event.Event):
+            return (not self.has_started) and event.type == pg.KEYDOWN and event.key == pg.K_SPACE
+        
+        def is_char(event: pg.event.Event):
+            return self.has_started and event.type == pg.KEYDOWN and event.unicode != ""
+
+        for event in pg.event.get():
+            if is_quit(event):
+                pg.quit()
+                sys.exit()
+            elif is_start(event):
+                self.start_exercise()
+            elif is_char(event):
+                self.handle_char_key(event.unicode)
+
+        # refresh chars_per_minute
+        if self.has_started and time.time() - self.last_cpm_refresh > self.options.CPM_COOLDOWN:
+            self.last_cpm_refresh = time.time()
+            # calculate chars per minute
+            delta_t = time.time() - self.time_start
+            self.chars_per_minute = 0 if delta_t == 0 else int( self.cursor / (delta_t/60) )
+
+        #print("get_mods", pg.key.get_mods() & pg.KMOD_NUM)
+        #print("self.is_mistyped", self.is_mistyped)
+            
         
         if self.has_started:
-            self.list_surf_text = self.seperate_text()
+            surface = self.textbox.render_exercise(self.cursor) 
         else:
-            self.list_surf_text = [
-                FONT_LARGE.render(self.text, True, self.COLOR_TEXT_NORMAL)
-            ]
-            
+            surface = self.textbox.render_normal(self.TEXT_START)
+        self.textbox.draw(self.screen, surface)
+
+
+    def handle_char_key(self, char: str):
+        if char == self.text_exercise[self.cursor]:
+            # correct key
+            if self.cursor == len(self.text_exercise) - 1:
+                self.textbox.render_normal(self.TEXT_FINISH)
+                self.has_started = False
+            self.cursor += 1
+            self.is_mistyped = False
+        else:
+            # mistyped
+            self.is_mistyped = True
+            self.errors += 1
+
+    def start_exercise(self):
+        self.has_started = True
+        self.text_exercise = self.text_exercise
+        self.time_start = time.time()
     
-    def seperate_text(self):
-        list_surf_text = []
-        
-        ### LEFT TEXT SURFACE
-        # index_left_left describes the index of the most left char of the left side text
-        # Right at the beginning, that index would be negative, since index 0 is in the center
-        
-        index_left_left_theoretical = self.cursor - int(self.LEN_TEXT_DISPLAYED/2)
-        # if too far at the beginning, add placeholder spaces to the text
-        spaces = ' ' * max(0, -index_left_left_theoretical)
-        
-        index_left_left = max(0, index_left_left_theoretical)
-        index_left_right = self.cursor - 1
-        list_surf_text.append(
-            FONT_LARGE.render(spaces + self.text[index_left_left:index_left_right+1], True, self.COLOR_TEXT_NORMAL)
-        )
-        
-        ### CENTER TEXT SURFACE
-        FONT_LARGE.set_underline(True)
-        list_surf_text.append(
-            FONT_LARGE.render(self.text[self.cursor], True, self.COLOR_TEXT_ACTIVE if not self.is_mistyped else self.COLOR_TEXT_MISTYPED)
-        )
-        FONT_LARGE.set_underline(False)
-        
-        ### RIGHT TEXT SURFACE
-        index_right_left = self.cursor+1
-        index_right_right = self.cursor+int(self.LEN_TEXT_DISPLAYED/2)
-        list_surf_text.append(
-            FONT_LARGE.render(self.text[index_right_left:index_right_right+1], True, self.COLOR_TEXT_NORMAL)
-        )
-        
-        return list_surf_text
-    
-    
-    def draw(self, surf):
-        # draw all surfaces from list_surf_text one after another
-        width = self.PADDING_X
-        for surf_text in self.list_surf_text:
-            surf.blit(surf_text, (self.RECT.x+width, self.RECT.y+self.PADDING_Y))
-            width += surf_text.get_width()
-        pg.draw.rect(surf, self.COLOR_RECT, self.RECT, 2)
 
 
 def main():
-    clock = pg.time.Clock()
-    textbox = TextBox(exercise_text)
-    
-    while True:
-        events = pg.event.get()
-        for event in events:
-            if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
-                pg.quit()
-                sys.exit()
-        
-        textbox.update(events)
-        WIN.fill(pg.Color('white'))
-        textbox.draw(WIN)
-        
-        WIN.blit(FONT_SMALL.render('Errors: %d' % textbox.errors, True, textbox.COLOR_TEXT_NORMAL), (textbox.RECT.x, textbox.RECT.bottom+50))
-        WIN.blit(FONT_SMALL.render('Cpm: %s' % textbox.cpm, True, textbox.COLOR_TEXT_NORMAL), (textbox.RECT.x + 160, textbox.RECT.bottom+50))
-        
-        pg.display.update()
-        
-        clock.tick(60)
+    exercise_text = r'Returns the dimensions needed to render the text. This can be used to help determine the positioning needed for text before it is rendered. It can also be used for wordwrapping and other layout effects.'
+
+    options = GameOptions()
+    screen = pg.display.set_mode((1200, 640))
+    game = Game(exercise_text, screen, options)
+    game.start()
 
 if __name__ == '__main__':
     main()
